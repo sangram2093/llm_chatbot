ce68a40c763f8c7ef17c51f8157b5996f83ffbd3
sangram2093 <sangram2093@gmail.com>
2025-05-09 23:45:47 +0530

    Update ai_assistant.py

--- ai_assistant.py
+++ ai_assistant.py
@@ -1,22 +1,25 @@
 import os
 import html
+import re
+from flask import Flask, request, render_template
 from pathlib import Path
 import vertexai
 from vertexai.generative_models import GenerativeModel
 import google.auth
 from dotenv import load_dotenv
-load_dotenv()
-import time
-from flask import Flask, request, render_template, jsonify
 from langchain.text_splitter import CharacterTextSplitter
-import markdown
-import re
+import markdown2
 
+# Load environment
+load_dotenv()
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('KEYFILE_PATH')
 credentials, project = google.auth.default()
+
+# Vertex AI init
 vertexai.init(project=os.getenv('PROJECT_NAME'), location=os.getenv('LOCATION'), credentials=credentials)
 multimodal_model = GenerativeModel(os.getenv("GENMIN_MODEL"))
 
+# Safety config
 safety_config = {
     vertexai.generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: vertexai.generative_models.HarmBlockThreshold.BLOCK_NONE,
     vertexai.generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: vertexai.generative_models.HarmBlockThreshold.BLOCK_NONE,
@@ -24,7 +27,7 @@ safety_config = {
     vertexai.generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: vertexai.generative_models.HarmBlockThreshold.BLOCK_NONE,
 }
 
-app = Flask(__name__, template_folder='/apps/abcd/sangram/code/templates')  # Specify template folder
+app = Flask(__name__, template_folder='/apps/abcd/sangram/code/templates')
 
 def summarize_code(code, prompt):
     max_chunk_size = 15000
@@ -47,17 +50,16 @@ def summarize_chunk(chunk, prompt, previous_summary):
     chunk = html.escape(chunk)
     full_prompt = prompt.format(chunk)
     response = multimodal_model.generate_content([full_prompt], safety_settings=safety_config)
-    response_text = re.sub(r'[\*\$\|\s]', '', response)
-    return response_text
+    return response.text
 
 @app.route('/', methods=['GET', 'POST'])
 def chat():
+    response_html = None
     if request.method == 'POST':
         user_input = request.form['prompt']
-        response = summarize_code('', user_input)  # Assuming you want to use the summarization for chat
-        response_html = markdown.markdown(response)
-        return render_template('ai_assistant.html', user_input=user_input, response=response_html)
-    return render_template('ai_assistant.html')
+        response_text = summarize_code('', user_input)
+        response_html = markdown2.markdown(response_text, extras=["fenced-code-blocks", "code-friendly", "tables"])
+    return render_template('ai_assistant.html', response=response_html)
 
 if __name__ == "__main__":
     host = "abcd.dg.dr.com"
